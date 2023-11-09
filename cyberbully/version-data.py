16222f34ffafda55d1fc3d9e2e630c8889029b54
Charleschua <charleschua19@gmail.com>
2023-11-09 15:06:06 +0800

    added functionality to initialize DVC

--- cyberbully/version-data.py
+++ cyberbully/version-data.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+
 from cyberbully.config_schemas.config_schema import Config
 from cyberbully.utils.config_utils import get_config
+from cyberbully.utils.data_utils import initialize_dvc
+from cyberbully.utils.utils import get_logger
 
 
 @get_config(config_path="../configs", config_name="config")
-def entrypoint(config: Config) -> None:
-    print(config)
+def version_data(config: Config) -> None:
+    # logger = get_logger(Path(__file__).name)
+    # logger.info()
+    
+    initialize_dvc()
 
 
 if __name__ == "__main__":
-    entrypoint()  # type: ignore
+    version_data()  # type: ignore
